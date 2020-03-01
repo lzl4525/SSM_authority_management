d72852c19be90a05fe3dd5fd4fc209499472979f
zl4525 <lzl4525@163.com>
2020-03-01 13:11:47 +0800

    通用工具包

---
+++ ssm_utils/src/main/java/com/gdut/ssm/utils/BCryptPasswordEncoderUtils.java
@@ -0,0 +1,20 @@
+package com.gdut.ssm.utils;
+
+import org.apache.ibatis.annotations.Select;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BCryptPasswordEncoderUtils {
+    private static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    public static String encodePassword(String password){
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    public static void main(String[] args) {
+        String password="123";
+        String pwd = encodePassword(password);
+        System.out.println("---------");
+        System.out.println(pwd);
+        System.out.println("------------");
+    }
+
+}
